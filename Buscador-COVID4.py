63bea73c532aa820ef3108021b42f498b9fa0585
Vicmaister97 <victorgarciacarrera@gmail.com>
2020-05-20 15:28:25 +0200

    v4.0 FUNCIONAL y bastante completa, ESCRITURA CORRECTA en 2 ficheros con tiempos de busqueda por grupos

--- Buscador-COVID4.py
+++ Buscador-COVID4.py
@@ -25,8 +25,6 @@ import numpy as np
 from concurrent.futures import ProcessPoolExecutor
 
 POOLSIZE = 10
-tiempos1 = []
-tiempos2 = []
 
 
 #@#@#@@ VER OTROS __MOTORES__ de BUSQUEDA (no navegadores, que solo es para visualizacion y navegar)
@@ -44,8 +42,6 @@ def main():
 
 	# Declaramos la var global POOLSIZE que indica el tamanio del pool de procesos
 	global POOLSIZE
-	global tiempos1
-	global tiempos2
 
 	# Lista donde guardamos los drivers que utilizamos para las busquedas
 	driver_list = []
@@ -72,6 +68,8 @@ def main():
 	# Aniadimos los drivers para realizar las busquedas
 	driver_list.append(driver1)
 	driver_list.append(driver2)
+
+	print("\n***\tINICIALIZADOS DRIVER\t***\n")
 	
 	""" Variables de busquedas y resultados """	
 	busquedas_groups = []		#  Lista con las busquedas agrupadas
@@ -137,10 +135,12 @@ def main():
 	"""PARALELO!!!!"""
 	executor = ProcessPoolExecutor(POOLSIZE)
 
+	print("\n***\tCOMENZAMOS BUSQUEDA\t***\n")
+
 	try:
 		resultados_groups = list(executor.map(buscar, busquedas_groups))
 
-	except futures.process.BrokenProcessPool as e:		# Si falla alguna búsqueda
+	except concurrent.futures.process.BrokenProcessPool as e:		# Si falla alguna búsqueda
 		print('could not start new tasks: {}'.format(e))
 
 	for driver in driver_list:
@@ -148,43 +148,55 @@ def main():
 		driver.quit()
 
 
+	print("\n***\tFIN BUSQUEDA, ORDENAR RESULTADOS\t***\n")
+
+
 	# Recorremos la lista de resultados agrupando por drivers
 	for pos in range(0,len(resultados_groups)):
-		if pos%2==0:	# Pertenece al primer driver
-			resultados1.append(resultados_groups[pos][0])
-			resultados2.append(resultados_groups[pos][1])
-		if pos%2==1:	# Pertenece al segundo driver
-			resultados1.append(resultados_groups[pos][0])
-			resultados2.append(resultados_groups[pos][1])
+		resultados1.append(resultados_groups[pos][0])	# Pertenece al primer driver
+		resultados2.append(resultados_groups[pos][1])	# Pertenece al segundo driver
+
 
 	print(resultados1)
 	print("\n\nBREAK2\n\n")
 	print(resultados2)
+	print("\n\n")
 
-	# Quitamos los grupos de resultados
-	for arr in resultados1:
-		for res in arr:
-			resultados1_orden.append(res)	
-
-	for arr in resultados2_groups:
-		for res in arr:
-			resultados2_orden.append(res)	
-
-	print(resultados1_orden)
-	print("\n\nBREAK2\n\n")
-	print(resultados2_orden)
-
+	# Quitamos los grupos de resultados Y CALCULAMOS LA SUMA TOTAL DE TIEMPOS DE BUSQUEDA por grupo
 	tiempo1_total = 0
-	for times in tiempos1:
-		tiempo1_total+=times
+	for arr in resultados1:
+		# Quitamos el ultimo elemento de cada grupo de resultados que contiene el TIEMPO
+		tiempo_parcial = arr.pop()
+		tiempo1_total+=tiempo_parcial
+
+		for res in arr:
+			resultados1_orden.append(res)
+
+	# Sacamos el tiempo medio de busqueda por grupo
+	tiempo1_media = tiempo1_total/len(resultados1)
 
 	tiempo2_total = 0
-	for times in tiempos2:
-		tiempo2_total+=times
+	for arr in resultados2:
+		# Quitamos el ultimo elemento de cada grupo de resultados que contiene el TIEMPO
+		tiempo_parcial = arr.pop()
+		tiempo2_total+=tiempo_parcial
+
+		for res in arr:
+			resultados2_orden.append(res)
+
+	# Sacamos el tiempo medio de busqueda por grupo
+	tiempo2_media = tiempo2_total/len(resultados2)
+
+	print(resultados1_orden)
+	print("\n\nBREAK3\n\n")
+	print(resultados2_orden)
+
+	#print("\nTIEMPO1_TOT" + str(tiempo1_total) + "\n")
+	#print("\nTIEMPO2_TOT" + str(tiempo2_total) + "\n")
 
 	""" Escribimos en un fichero BUSQUEDA *.es: NUMERO DE RESULTADOS: 500"""
 	ahora = datetime.now()
-	nombre = str(ahora.day) + "-" + str(ahora.month) + "-" + str(ahora.year) + ".txt"
+	nombre = str(ahora.day) + "-" + str(ahora.month) + "-" + str(ahora.year) + "_v4_" + "GOOGLE" + ".txt"
 	"""if (ahora.hour < 14 and ahora.hour > 2):
 		nombre += " Morning" + ".txt"
 	else:
@@ -193,12 +205,15 @@ def main():
 	buscador = "GOOGLE"
 	res = resultados1_orden
 	tiempoTOT = tiempo1_total
+	tiempoGRUPO = tiempo1_media
 
 	for i in range(2):
 		if i==1:
 			buscador = "FIREFOX"
 			res = resultados2_orden
 			tiempoTOT = tiempo2_total
+			tiempoGRUPO = tiempo2_media
+			nombre = str(ahora.day) + "-" + str(ahora.month) + "-" + str(ahora.year) + "_v4_" + "FIREFOX" + ".txt"
 
 		f = open(nombre, "w+")
 		f.write("***\tBuscador v4.0 \t Archivo con el número de resultados obtenidos por término de búsqueda en " + buscador + "\t***")
@@ -209,14 +224,13 @@ def main():
 			linea = "\n" + busquedas[j] + "\t\t" + str(res[j])
 			f.write(linea)
 
-		f.write("\n\nTIEMPO DE BUSQUEDA: " + str(tiempoTOT))
+		f.write("\n\nTIEMPO DE BUSQUEDA(secuencial): " + str(tiempoTOT))
+		# Escribimos el tiempo medio de busqueda por grupo
+		f.write("\n\nTIEMPO MEDIO DE BUSQUEDA(por grupo): " + str(tiempoGRUPO))
 		f.close()
 
 
 def buscar(terminos):
-
-	global tiempos1
-	global tiempos2
 
 	res = []
 	res_driver = []
@@ -252,10 +266,14 @@ def buscar(terminos):
 	for driver in driver_list:
 		if i==1:
 			print("\nCAMBIO DE DRIVER EN LA BUSQUEDA\n\n")
+			# En res_driver tenemos los resultados de los terminos en 1 driver
+			# Aniadimos al final el tiempo que ha tardado ese driver
+			res_driver.append(busqueda_time)
 			res.append(res_driver)		# Aniadimos en la lista de resultados finales[res_driver1, res_driver2] 
 										#	los resultados del driver1
 			res_driver = []
-			tiempos1.append(busqueda_time)
+			#tiempos1.append(busqueda_time)
+			#print("\nTIEMPOS1: " + str(tiempos1))
 
 		## Inicio TIME
 		start_time = time.time()
@@ -294,11 +312,17 @@ def buscar(terminos):
 
 		## busqueda TIME
 		busqueda_time = time.time()-start_time
+		print("\n*\tTIEMPO BUSQUEDA: " + str(busqueda_time) +"\t*\n")
 		i += 1		# Para llevar la cuenta del driver
+
+	# En res_driver tenemos los resultados de los terminos en 2 driver
+	# Aniadimos al final el tiempo que ha tardado ese driver
+	res_driver.append(busqueda_time)
 
 	# Terminamos de aniadir la info del driver 2
 	res.append(res_driver)
-	tiempos2.append(busqueda_time)
+	#tiempos2.append(busqueda_time)
+	#print("\nTIEMPOS2: " + str(tiempos2))
 
 	""" Cerramos las pestaña abierta """
 	for driver in driver_list:
